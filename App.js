0387f2ce2839f3338de02b695430682051307f07
Matt Barraza-Carl <PerStirpes@users.noreply.github.com>
2018-05-31 12:29:16 -0700

    initial commit

---
+++ App.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { Util } from 'expo';
+
+import Loader from './Loader';
+
+export default class App extends React.Component {
+  state = {
+    appReady: false,
+    rootKey: Math.random(),
+  };
+
+  constructor() {
+    super();
+
+    this._image = require('./white-bg.png');
+  }
+
+  componentDidMount() {
+    this.resetAnimation();
+  }
+
+  resetAnimation() {
+    this.setState({
+      appReady: false,
+      rootKey: Math.random(),
+    });
+
+    setTimeout(() => {
+      this.setState({
+        appReady: true,
+      });
+    }, 2000);
+  }
+
+  render() {
+    return (
+      <View key={this.state.rootKey} style={styles.root}>
+        <Loader
+          isLoaded={this.state.appReady}
+          imageSource={this._image}
+          backgroundStyle={styles.loadingBackgroundStyle}
+        >
+          <View style={styles.container}>
+            <Button
+              onPress={() => {
+                this.resetAnimation();
+              }}
+              title="See it again"
+            />
+          </View>
+        </Loader>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 2,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  loadingBackgroundStyle: {
+    backgroundColor: 'black',
+  },
+});
