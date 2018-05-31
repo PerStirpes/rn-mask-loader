757a75d13a7d85f1b0b861da7a4972770a0bcea9
Matt Barraza-Carl <PerStirpes@users.noreply.github.com>
2018-05-31 13:24:13 -0700

    Add navigation

--- App.js
+++ App.js
@@ -1,72 +1,82 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
-import { Util } from 'expo';
+import { createSwitchNavigator } from 'react-navigation';
 
 import Loader from './components/Loader';
+import SplashScreen from './screens/Splash';
+import HomeScreen from './screens/Home';
 
-export default class App extends React.Component {
-  state = {
-    appReady: false,
-    rootKey: Math.random(),
-  };
+// class App extends React.Component {
+//   state = {
+//     appReady: false,
+//     rootKey: Math.random(),
+//   };
 
-  constructor() {
-    super();
+//   constructor() {
+//     super();
 
-    this._image = require('./images/white-bg.png');
-  }
+//     this._image = require('./images/white-bg.png');
+//   }
 
-  componentDidMount() {
-    this.resetAnimation();
-  }
+//   componentDidMount() {
+//     this.resetAnimation();
+//   }
 
-  resetAnimation() {
-    this.setState({
-      appReady: false,
-      rootKey: Math.random(),
-    });
+//   resetAnimation() {
+//     this.setState({
+//       appReady: false,
+//       rootKey: Math.random(),
+//     });
 
-    setTimeout(() => {
-      this.setState({
-        appReady: true,
-      });
-    }, 2000);
-  }
+//     setTimeout(() => {
+//       this.setState({
+//         appReady: true,
+//       });
+//     }, 2000);
+//   }
 
-  render() {
-    return (
-      <View key={this.state.rootKey} style={styles.root}>
-        <Loader
-          isLoaded={this.state.appReady}
-          imageSource={this._image}
-          backgroundStyle={styles.loadingBackgroundStyle}
-        >
-          <View style={styles.container}>
-            <Button
-              onPress={() => {
-                this.resetAnimation();
-              }}
-              title="See it again"
-            />
-          </View>
-        </Loader>
-      </View>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <View key={this.state.rootKey} style={styles.root}>
+//         <Loader
+//           isLoaded={this.state.appReady}
+//           imageSource={this._image}
+//           backgroundStyle={styles.loadingBackgroundStyle}
+//         >
+//           <View style={styles.container}>
+//             <Button
+//               onPress={() => this.props.navigation.navigate('Home')}
+//               title="Home"
+//             />
+//           </View>
+//         </Loader>
+//       </View>
+//     );
+//   }
+// }
 
-const styles = StyleSheet.create({
-  root: {
-    flex: 2,
+// const styles = StyleSheet.create({
+//   root: {
+//     flex: 1,
+//   },
+//   container: {
+//     flex: 1,
+//     backgroundColor: 'white',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+
+//   loadingBackgroundStyle: {
+//     backgroundColor: 'hsl(0, 0%, 0%)',
+//   },
+// });
+
+export default (Switch = createSwitchNavigator(
+  {
+    Splash: SplashScreen,
+    Home: HomeScreen,
   },
-  container: {
-    flex: 1,
-    backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
+  {
+    initialRouteName: 'Splash',
   },
-
-  loadingBackgroundStyle: {
-    backgroundColor: 'black',
-  },
-});
+));
